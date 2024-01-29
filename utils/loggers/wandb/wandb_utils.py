194fbdaf87a23d3388d41aefa57e17bff179dd1e
GilSimas <vascodvrodrigues@gmail.com>
2024-01-29 14:54:08 +0000

    added changes to wandb logger

--- utils/loggers/wandb/wandb_utils.py
+++ utils/loggers/wandb/wandb_utils.py
@@ -66,14 +66,10 @@ class WandbLogger:
         self.data_dict = None
         if self.wandb:
             self.wandb_run = wandb.run or wandb.init(
-                config=opt,
-                resume="allow",
+                
                 project="YOLOv5" if opt.project == "runs/train" else Path(opt.project).stem,
-                entity=opt.entity,
                 name=opt.name if opt.name != "exp" else None,
                 job_type=job_type,
-                id=run_id,
-                allow_val_change=True,
             )
 
         if self.wandb_run and self.job_type == "Training":
