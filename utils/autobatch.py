eacfc48a87ff6ae5e3c49393cccd06db7ebdc9eb
Victoria Oberascher <vicioberascher@gmail.com>
2025-03-19 12:18:20 +0100

    add debug messages for autobatch

--- utils/autobatch.py
+++ utils/autobatch.py
@@ -43,6 +43,8 @@ def autobatch(model, imgsz=[640, 640], fraction=0.8, batch_size=16):
     # Inspect CUDA memory
     gb = 1 << 30  # bytes to GiB (1024 ** 3)
     d = str(device).upper()  # 'CUDA:0'
+    print(f"device in autobatch: {d}")
+    properties = torch.cuda.get_device_properties(1)  # device properties
     properties = torch.cuda.get_device_properties(device)  # device properties
     t = properties.total_memory / gb  # GiB total
     r = torch.cuda.memory_reserved(device) / gb  # GiB reserved
