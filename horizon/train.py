4292b7fd630b10b310bc4feee5eac6409d829172
Victoria Oberascher <vicioberascher@gmail.com>
2025-03-19 11:05:23 +0100

    final last try

--- horizon/train.py
+++ horizon/train.py
@@ -233,7 +233,8 @@ def evaluate(
 
     return v_loss, v_ploss, v_tloss, mse_pitch, mse_theta
 
-def _log_device_info():
+def _log_device_info(device):
+    print("Device Info:", device)
     print("CUDA Available:", torch.cuda.is_available())
     print("GPU Count:", torch.cuda.device_count())
 
@@ -254,7 +255,7 @@ def _setup_device(device_spec):
         
        
     # Return either "cpu" or "cuda:X" format
-    return "cpu" if device == "cpu" else str(device)
+    return "cpu" if device == "cpu" else f"cuda:{device}"
 
 
 def run(
@@ -299,7 +300,7 @@ def run(
     # configure device
     device = _setup_device(device)
 
-    _log_device_info()
+    _log_device_info(device)
 
     # ensure that imgsz is a tuple (height, width)
     imgsz = tuple([imgsz] * 2) if isinstance(imgsz, int) else imgsz
