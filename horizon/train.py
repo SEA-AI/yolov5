fca4243499b80df186e6583e4686d8a40dfcf512
Victoria Oberascher <vicioberascher@gmail.com>
2025-03-19 14:46:18 +0100

    CV-310: just allow downscaling in multiscale

--- horizon/train.py
+++ horizon/train.py
@@ -128,7 +128,7 @@ def update(
         if multi_scale:
             imgsz = model.imgsz
             gs = max(int(model.stride.max()), 32)  # grid size (max stride)
-            sz = random.randrange(int(imgsz * 0.5), int(imgsz * 1.5) + gs) // gs * gs  # size
+            sz = random.randrange(int(imgsz * 0.5), int(imgsz) + gs) // gs * gs  # size
             sf = sz / max(images.shape[2:])  # scale factor
             if sf != 1:
                 ns = [math.ceil(x * sf / gs) * gs for x in images.shape[2:]]  # new shape (stretched to gs-multiple)
