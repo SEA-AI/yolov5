1d9f6c6a3ea76a18404c78984f2c90e286b6f0c0
hichem-sea <hichem.abdellali@sea.ai>
2025-05-26 13:29:24 +0200

    update dposctring

--- export_ahoy.py
+++ export_ahoy.py
@@ -1,5 +1,5 @@
 """
-Export AHOY or AHOY with Horizon-OBB to ONNX format.
+Export AHOY or AHOYOBB( with Horizon-OBB) to ONNX format.
 
 ONNX is an open standard for machine learning models that enables interoperability 
 between different frameworks and platforms.
@@ -12,7 +12,7 @@ Example:
     # Using local weights files:
     python export_ahoy.py \
         --det-weights yolov5n.pt \
-        --hor-weights yolov5h.pt \
+        --hor-weights yolov11n-obb.pt \
         --imgsz 640 \
         --infsz 320
         --batch-size 2 \
@@ -23,7 +23,7 @@ Example:
 
 
 # flatten this
-    python export_ahoy.py --det-weights yolov5n.pt --hor-weights yolov5h.pt --imgsz 640 --infsz 320 --batch-size 2 --fuse --half --fname ahoy.onnx
+    python export_ahoy.py --det-weights yolov5n.pt --hor-weights yolov11n-obb.pt --imgsz 640 --infsz 320 --batch-size 2 --fuse --half --fname ahoy.onnx --obb
 
 
     # Using W&B artifacts:
