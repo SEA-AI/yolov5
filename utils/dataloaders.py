83522179d9f4b1ff20e4767556e8bb32e8f2a720
kevinconka <kserranov@gmail.com>
2025-07-10 22:41:51 +0200

    Update LoadImagesAndLabels.load_mosaic
    - include padding parameters for transforming normalized xywh to pixel xyxy format and for segment coordinates

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -955,8 +955,8 @@ class LoadImagesAndLabels(Dataset):
             padh = y1a - y1b
 
             if labels.size:
-                labels[:, 1:] = xywhn2xyxy(labels[:, 1:], w, h)
-                segments = [xyn2xy(x, w, h) for x in segments]
+                labels[:, 1:] = xywhn2xyxy(labels[:, 1:], w, h, padw, padh)  # normalized xywh to pixel xyxy format
+                segments = [xyn2xy(x, w, h, padw, padh) for x in segments]
 
             # Labels
             labels4.append(labels)
