faf52c61187e058470c2e78ae16915688508c411
kevinconka <kserranov@gmail.com>
2025-07-15 08:10:02 +0200

    Remove deprecated training scripts and weight configuration files

--- scripts/evolve.sh
+++
@@ -1,9 +0,0 @@
-MODEL=yolov5s
-/home/jigglypuff/anaconda3/envs/yolo/bin/python /home/jigglypuff/GitHub/yolov5-extended/train.py \
-    --img 640 \
-    --batch 64 \
-    --epochs 10 \
-    --data /home/jigglypuff/GitHub/yolov5-extended/datasets/RGB_127k_21-03-2023/dataset.yaml \
-    --weights ${MODEL}.pt \
-    --name RGB_127k_evolve_${MODEL} \
-    --evolve
