ae90bb4813198eba787bbae121a094766cb861cb
gilseamas <gil.simas@sea.ai>
2025-08-16 22:33:57 +0100

    lol

--- train_ga.py
+++ train_ga.py
@@ -37,6 +37,7 @@ def train_and_validate(gene_ranges, individual, device_id, return_dict, i, proje
         for gene_name, gene_value in zip(gene_ranges.keys(), individual):
             if (gene_ranges[gene_name][-1] == "log"):
                 if gene_value < math.exp(gene_ranges[gene_name][0]) or gene_value > math.exp(gene_ranges[gene_name][1]):
+                    print(gene_value, math.exp(gene_ranges[gene_name][0]), math.exp(gene_ranges[gene_name][1]))
                     raise ValueError(f"Invalid value {gene_value} for gene {gene_name}")
                 gene_value = math.pow(10, gene_value)
             elif gene_ranges[gene_name][-1] == "linear":
