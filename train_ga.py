41498ec7df350182b5ae3b77af029b9b4819b144
gilseamas <gil.simas@sea.ai>
2025-08-16 22:22:15 +0100

    debug

--- train_ga.py
+++ train_ga.py
@@ -180,7 +180,7 @@ class GeneticAlgorithm:
                     gene_value = int(gene_value)
                 genome.append(gene_value)
             else:
-                print(type(ranges), ranges, "wtf", ranges[-1], type(ranges[-1]))
+                print(type(ranges), ranges, "wtf", ranges[-1], type(ranges[-1]), gene)
                 raise ValueError(f"Invalid distribuition type: {ranges[-1]}")
         return genome
 
