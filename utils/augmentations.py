a761236a6fddc484c43eab29b082e1fb25376199
Daniel Fortunato <daniel.fortunato@bsb-ai.com>
2025-12-12 16:33:33 +0000

    new gamma, blur and clahe augmentations

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -34,18 +34,19 @@ class Albumentations:
 
             T = [
                 A.RandomResizedCrop(height=size, width=size, scale=(0.8, 1.0), ratio=(0.9, 1.11), p=0.0),
-                A.Blur(p=0.01),
+                A.Blur(blur_limit=3, p=0.3),
                 A.MedianBlur(p=0.01),
+                A.GaussNoise(std_range=[0.1, 0.2], p=0.5),
                 A.MotionBlur(blur_limit=(12, 30), angle_range=(0, 0), direction_range=(-1.0, 1.0), p=hyp.get("motion_blur", 0.0) if hyp else 0.0),
                 A.ToGray(p=0.01),
-                A.CLAHE(p=0.01),
+                A.CLAHE(clip_limit=4.0, tile_grid_size=(8, 8), p=0.5),
                 A.RandomBrightnessContrast(
                     brightness_limit=(-0.6, -0.3),   
                     contrast_limit=(0.3, 0.6),
                     brightness_by_max=True,   # Essential for thermal data
                     p=0.8                     # High probability due to thermal variation
                 ),
-                A.RandomGamma(p=0.0),
+                A.RandomGamma(gamma_limit=(150, 300), p=0.5),
                 A.ImageCompression(p=hyp.get("compression", 0.0) if hyp else 0.0, quality_lower=50),
             ]  # transforms
             self.transform = A.Compose(T, bbox_params=A.BboxParams(format="yolo", label_fields=["class_labels"]))
